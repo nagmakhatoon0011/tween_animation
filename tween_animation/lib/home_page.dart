79b685ad2d8155dd45fed3e6ae7ac71c9f61da46
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-06-13 14:54:37 +0530

    change

--- tween_animation/lib/home_page.dart
+++ tween_animation/lib/home_page.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
         child: Scaffold(
          body: Column(
            children: [
-             Text("dfghjkl")
+             Text("nagma")
            ],
          ),
 
--- tween_animation/lib/main.dart
+++ tween_animation/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tween_animation/home_page.dart';
-
+void main(){
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
