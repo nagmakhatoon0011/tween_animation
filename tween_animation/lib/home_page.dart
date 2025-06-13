b7d2ac630b330cc5fef9c969681bdd5d314bccf8
nagmakhatoon0011 <nagmaedugaon786@gmail.com>
2025-06-13 14:43:11 +0530

    change

--- tween_animation/lib/home_page.dart
+++ tween_animation/lib/home_page.dart
@@ -10,34 +10,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  late AnimationController controller;
-  late Animation<double> sizeAnimation;
   @override
-  void initState(){
-    super.initState();
-    controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-    opacityAnimation = Tween<double>(begin: 0.0,end: 100.0),
-    sizeAnimation = Tween<double>(begin: 0.0,end: 100.0);
-  }
+ 
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(backgroundColor: Colors.green,),
-          body:Center(
-            child: AnimatedBuilder(animation: sizeAnimation, builder: (context, child) {
-              return Container(
-                height: sizeAnimation.value,
-                width: sizeAnimation.value,
-                child: child,
-
-              );
-            },
-              child: FlutterLogo(),
-            ),
-          )
+         body: Column(
+           children: [
+             Text("dfghjkl")
+           ],
+         ),
 
         ));
   }
